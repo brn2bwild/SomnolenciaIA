45edc3ad248d8a2af2fa1ab87aa4d1dae732e1bb
brn_2_b_wild@hotmail.com <brn2bwild>
2025-12-01 21:04:55 +0200

    Modificada la versión de python y optimizado el inicio de la cámara.

--- detec.py
+++ detec.py
@@ -1,10 +1,15 @@
 import math
 import time
-import os
-os.environ["OPENCV_VIDEOIO_MSMF_ENABLE_HW_TRANSFORMS"] = "0"
 import cv2
 import mediapipe as mp
 import numpy as np
+import os
+
+def clearConsole():
+    command = "clear"
+    if os.name in ("nt", "dos"):
+        command = "cls"
+    os.system(command)
 
 PARPADEO = False
 CONTEO = 0
@@ -14,38 +19,30 @@ FINAL = 0
 CONTEO_SUE = 0
 MUESTRA = 0
 
-face_mesh = mp.solutions.face_mesh.FaceMesh(
-    max_num_faces=1,
-    refine_landmarks=True,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5,
-)
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh(refine_landmarks=True, max_num_faces=1)
 
 left_eye_idx = [362, 385, 387, 263, 373, 380]  # puntos ojo izquierdo
 right_eye_idx = [33, 160, 158, 133, 153, 144]  # puntos ojo derecho
 
+
 def calcular_ear(ojo):
     p1, p2, p3, p4, p5, p6 = ojo  # puntos de los ojos mediante un array
     # distancias euclidianas np.linalg.norm()
-    distancia1 = math.sqrt((p2[0] - p6[0]) ** 2 + (p2[1] - p6[1]) ** 2)  # p2 -- p6
-    distancia2 = math.sqrt((p3[0] - p5[0]) ** 2 + (p3[1] - p5[1]) ** 2)  # p3 -- p5
-    distancia3 = math.sqrt((p1[0] - p4[0]) ** 2 + (p1[1] - p4[1]) ** 2)  # p1 -- p4
+    distancia1 = math.sqrt((p2[0] - p6[0]) * 2 + (p2[1] - p6[1]) * 2)  # p2 -- p6
+    distancia2 = math.sqrt((p3[0] - p5[0]) * 2 + (p3[1] - p5[1]) * 2)  # p3 -- p5
+    distancia3 = math.sqrt((p1[0] - p4[0]) * 2 + (p1[1] - p4[1]) * 2)  # p1 -- p4
 
     ear = (distancia1 + distancia2) / (2.0 * distancia3)
-    print(ear, end="\r")
     return ear
 
+
 camara = cv2.VideoCapture(0)
-
-if not camara.isOpened():
-    print("Failed to open capture device")
-    exit(1)
-
 while True:
     ok, frame = camara.read()
     if not ok:
         break
-    # frame = cv2.read(frame)
+    frame = cv2.flip(frame, 1)
     alto, ancho, _ = frame.shape
 
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -59,12 +56,11 @@ while True:
         return nuevosPuntos
     """
     if resultados.multi_face_landmarks:
+        print(f"Rostro detectado = {True}", end="\r")
         landmarks = resultados.multi_face_landmarks[0].landmark
         ojo_izq = []
         ojo_der = []  # iran las coordenadas convertidas en puntos
         # usar los array de los ojos
-
-        # print("rostro detectado", end="\r")
 
         for i in left_eye_idx:
             x = int(landmarks[i].x * ancho)
@@ -81,11 +77,11 @@ while True:
 
         EAR_promedio = (EAR_izq + EAR_der) / 2.0
 
-        # for punto in ojo_izq:
-        #     cv2.circle(frame, punto, 2, (0, 255, 0), -1)
+        for punto in ojo_izq:
+            cv2.circle(frame, punto, 2, (0, 255, 0), -1)
 
-        # for punto in ojo_der:
-        #     cv2.circle(frame, punto, 2, (0, 255, 0), -1)
+        for punto in ojo_der:
+            cv2.circle(frame, punto, 2, (0, 255, 0), -1)
         """
         if EAR_promedio > 0.17:
             color = (0, 255, 0)  # verde si ojo abierto
@@ -96,6 +92,8 @@ while True:
         """
         # texto = f"EAR: {EAR_promedio:.2f}"
         # cv2.putText(frame, texto, (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 2)
+        
+        #se puso en comentario el 25/11/25
 
         # cv2.putText(
         #     frame,
@@ -124,11 +122,12 @@ while True:
         #     (0, 255, 0),
         #     2,
         # )
-
+        
         if EAR_izq <= 0.17 and EAR_der <= 0.17 and PARPADEO is False:
             CONTEO += 1
             PARPADEO = True
             INICIO = time.time()
+            clearConsole()
         elif EAR_izq > 0.17 and EAR_der > 0.17 and PARPADEO is True:
             PARPADEO = False
             FINAL = time.time()
@@ -139,8 +138,9 @@ while True:
             MUESTRA = TIEMPO
             INICIO = 0
             FINAL = 0
+            print("Microsueño = True")
 
-    # cv2.imshow("ojos", frame)
+    #cv2.imshow("ojos", frame)
     if 0xFF == ord("q"):
         break
 camara.release()
